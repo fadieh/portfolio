3f72c80135d09e2cd3dc756b4fee44653276bcb2
Fadie Hannona <fadiehannona@gmail.com>
2014-11-20 20:14:37 +0000

    Added pages, contact page as well

--- app/server.rb
+++ app/server.rb
@@ -5,3 +5,19 @@ set :public_folder, Proc.new { File.join(root, '..', 'public') }
 get '/' do
 	erb :index
 end
+
+get '/story' do
+	erb :story
+end
+
+get '/web' do
+	erb :web
+end
+
+get '/design' do
+	erb :design
+end
+
+get '/contact' do
+	erb :contact
+end
