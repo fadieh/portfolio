3c196a05256cb75a03fee038d4d0db38c39c404a
Fadie Hannona <fadiehannona@gmail.com>
2014-12-28 18:06:58 +0000

    last for the day

--- app/server.rb
+++ app/server.rb
@@ -1,16 +1,15 @@
 require 'sinatra'
 
 set :public_folder, Proc.new { File.join(root, '..', 'public') }
-set :views, Proc.new { File.join(root, '..', 'views') }
 
-get '/' do
-	erb :index
-end
+	get '/' do
+		erb :index
+	end
 
-get '/story' do
-	erb :story
-end
+	get '/story' do
+		erb :story
+	end
 
-get '/contact' do
-	erb :contact
-end
+	get '/contact' do
+		erb :contact
+	end
